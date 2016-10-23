17ad107b97848c54c1ba694126e6266ff683c04f
Marcin <marcin.szafranek@gmail.com>
2016-10-23 13:39:38 +0200

    Add timeouts for Chrome Browser

--- src/test/java/gui/junitparams/utils/ChromeBrowser.java
+++ src/test/java/gui/junitparams/utils/ChromeBrowser.java
@@ -2,16 +2,18 @@ package gui.junitparams.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by E5410 on 2016-10-23.
  */
 
 public class ChromeBrowser {
-        protected static WebDriver driver;
+    public WebDriver driver;
 
-    protected static WebDriver getURL (String url) {
+    public WebDriver getURL (String url) {
         driver =new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get(url);
         return driver;
