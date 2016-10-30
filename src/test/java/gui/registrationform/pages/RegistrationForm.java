319d4efa96e331513de1f0d5cf76579079eb5d0d
MSzafranek <marcin.szafranek>
2016-10-30 18:29:35 +0100

    Add method for send value to field: age, comment, email, phone

--- src/test/java/gui/registrationform/pages/RegistrationForm.java
+++ src/test/java/gui/registrationform/pages/RegistrationForm.java
@@ -9,10 +9,40 @@ import org.openqa.selenium.WebElement;
  */
 public class RegistrationForm {
 
+    public void sendValueToAgeField(WebDriver driver, int age) {
+        WebElement field = driver.findElement(By.id("age"));
+        field.clear();
+        field.sendKeys(String.valueOf(age));
+    }
+
+    public void sendValueToCommentField(WebDriver driver, String comment) {
+        WebElement field = driver.findElement(By.id("comment"));
+        field.clear();
+        field.sendKeys(comment);
+    }
+
+    public void sendValueToEmailField(WebDriver driver, String email) {
+        WebElement field = driver.findElement(By.id("email"));
+        field.clear();
+        field.sendKeys(email);
+    }
+
     public void sendValueToNameField(WebDriver driver, String name) {
         WebElement field = driver.findElement(By.id("name"));
         field.clear();
         field.sendKeys(name);
+    }
+
+    public void sendValueToPhoneField(WebDriver driver, String phone) {
+        WebElement field = driver.findElement(By.id("phone"));
+        field.clear();
+        field.sendKeys(phone);
+    }
+
+    public void sendValueToSurnameField(WebDriver driver, String surname) {
+        WebElement field = driver.findElement(By.id("surname"));
+        field.clear();
+        field.sendKeys(surname);
     }
 
     public void pressSubmitButton(WebDriver driver) {
