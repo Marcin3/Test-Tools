8a89c053fb1e164d27dfb0270e1c95c223054ca7
MSzafranek <marcin.szafranek>
2016-10-30 17:24:50 +0100

    Add Before and After Class

--- src/test/java/gui/registrationform/test/RegistrationFormTest.java
+++ src/test/java/gui/registrationform/test/RegistrationFormTest.java
@@ -1,7 +1,25 @@
 package gui.registrationform.test;
 
+import gui.registrationform.pages.RegistrationForm;
+import gui.registrationform.utils.ChromeBrowser;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
 /**
  * Created by marcin on 30/10/16.
  */
-public class RegistrationFormTest {
+public class RegistrationFormTest extends RegistrationForm {
+    public static WebDriver driver;
+    private static final String baseUrl = "http://www.websiteuat.example.org/";
+
+    @BeforeClass
+    public static void init() {
+        ChromeBrowser browser = new ChromeBrowser();
+        driver = browser.getDriverWithURL(baseUrl);
+    }
+
+    @AfterClass
+    public static void tearDown() { driver.close();}
+
+
 }
