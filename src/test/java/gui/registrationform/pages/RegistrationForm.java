47893f529e7cd641f3f090774d7b3cc3b576871a
MSzafranek <marcin.szafranek>
2016-10-30 17:24:12 +0100

    Add methods for name field and submit button

--- src/test/java/gui/registrationform/pages/RegistrationForm.java
+++ src/test/java/gui/registrationform/pages/RegistrationForm.java
@@ -1,7 +1,21 @@
 package gui.registrationform.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by marcin on 30/10/16.
  */
 public class RegistrationForm {
+
+    public void sendValueToNameField(WebDriver driver, String name) {
+        WebElement field = driver.findElement(By.id("name"));
+        field.clear();
+        field.sendKeys(name);
+    }
+
+    public void pressSubmitButton(WebDriver driver) {
+        driver.findElement(By.id("submit")).click();
+    }
 }
